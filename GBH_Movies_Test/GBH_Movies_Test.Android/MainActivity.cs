42d623d454e15ab31eaa7c0ec4371634700fb7ac
Sergio Joel Ferreras <sergio.Ferreras@isgwebsite.com>
2018-08-18 08:38:42 -0400

    Feat: SplashScreen added.

--- GBH_Movies_Test/GBH_Movies_Test.Android/MainActivity.cs
+++ GBH_Movies_Test/GBH_Movies_Test.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace GBH_Movies_Test.Droid
 {
-    [Activity(Label = "GBH_Movies_Test", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "GBH_Movies_Test", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
