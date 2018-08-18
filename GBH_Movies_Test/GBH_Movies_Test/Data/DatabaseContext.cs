9055965b135594c247ee37d8dcc9c4308f0af18c
Sergio Joel Ferreras <sergio.Ferreras@isgwebsite.com>
2018-08-18 10:08:27 -0400

    ---- Repository Pattern implemented ---- Feat: DatabaseContext added. Feat: DBRepository added. Feat: IDBReposository added. Feat: EntityFrameworkCore.SqlLite nuget Package added.

---
+++ GBH_Movies_Test/GBH_Movies_Test/Data/DatabaseContext.cs
@@ -0,0 +1,50 @@
+﻿using GBH_Movies_Test.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Xamarin.Forms;
+
+namespace GBH_Movies_Test.Data
+{
+    /// <summary>
+    /// The DatabaseContext for the Database.
+    /// </summary>
+    /// <typeparam name="T">Any object that inherit from BaseEntity</typeparam>
+    public class DatabaseContext<T> : DbContext where T : BaseEntity 
+    {
+        public DbSet<T> Entity { get; set; }
+
+        string DbName = "GBH_Movies.db3";
+
+        public DatabaseContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder)
+        {
+            string path = "";
+
+            switch (Device.RuntimePlatform)
+            {
+                case Device.iOS:
+
+                    SQLitePCL.Batteries_V2.Init();
+
+                    path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "..", "Library", DbName);
+
+                    break;
+                case Device.Android:
+
+                    path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), DbName);
+
+                    break;
+
+                default:
+                    throw new NotImplementedException("Platform not supported");
+            }
+        }
+    }
+}
---
+++ GBH_Movies_Test/GBH_Movies_Test/Services/IDBRepository.cs
@@ -0,0 +1,21 @@
+﻿using GBH_Movies_Test.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GBH_Movies_Test.Services
+{
+    /// <summary>
+    /// DBRepository interface, it contains the CRUD Methods for the database. 
+    /// </summary>
+    /// <typeparam name="T">Any object that inherit from BaseEntity</typeparam>
+    public interface IDBRepository<T> where T : BaseEntity
+    {
+        Task<T> GetEntity(int id);
+        Task<IEnumerable<T>> GetEntities();
+        Task<bool> AddEntity(T obj);
+        Task<bool> UpdateEntity(T obj);
+        Task<bool> DeleteEntity(T obj);
+    }
+}
