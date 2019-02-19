e3c8f97476a7201ccf4a911b21729cd96ff1f6fa
Sergio Ferreras <Sergio_Ferreras_B@claro.com.do>
2019-02-19 11:53:32 -0400

    Refactor: Some Changes.

--- SSFR_Movies/SSFR_Movies/Helpers/ContentPageBase.cs
+++ SSFR_Movies/SSFR_Movies/Helpers/ContentPageBase.cs
@@ -1,4 +1,4 @@
-﻿using ReactiveUI;
+﻿using ReactiveUI.XamForms;
 using System;
 using System.Collections.Generic;
 using System.Text;
--- SSFR_Movies/SSFR_Movies/ViewModels/ViewModelBase.cs
+++ SSFR_Movies/SSFR_Movies/ViewModels/ViewModelBase.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ReactiveUI;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
@@ -11,13 +12,15 @@ namespace SSFR_Movies.ViewModels
     /// ViewModelBae: to make ease the implementation of INotifyPorpertyChanged in the properties of the ViewModels
     /// </summary>
     [Preserve(AllMembers = true)]
-    public class ViewModelBase : INotifyPropertyChanged
+    public class ViewModelBase : ReactiveObject 
     {
+#pragma warning disable CS0108 // Member hides inherited member; missing new keyword
         public event PropertyChangedEventHandler PropertyChanged;
+#pragma warning restore CS0108 // Member hides inherited member; missing new keyword
 
         public bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string name = null)
         {
-            if(Equals(storage, value))
+            if (Equals(storage, value))
                 return true;
 
             storage = value;
