a17815d90d24910d4a0f0d6c70dbb2c0d370ddb3
Sergio Joel Ferreras <sergiojoel9877@hotmail.com>
2019-02-24 22:25:08 -0400

    Feat: Add WebView Renderer.

--- SSFR_Movies/SSFR_Movies.Android/CustomRenderers/HybridWebViewRenderer.cs
+++ SSFR_Movies/SSFR_Movies.Android/CustomRenderers/HybridWebViewRenderer.cs
@@ -9,13 +9,71 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using SSFR_Movies.Droid.CustomRenderers;
+using SSFR_Movies.Helpers;
 using Xamarin.Forms;
+using SSFR_Movies.Droid;
+using Xamarin.Forms.Platform.Android;
+using SSFR_Movies.CustomRenderers;
+using Android.Webkit;
+using System.Threading.Tasks;
+using Java.Lang;
 
 [assembly: ExportRenderer(typeof(HybridWebView), typeof(HybridWebViewRenderer))]
 namespace SSFR_Movies.Droid.CustomRenderers
 {
-    public class HybridWebViewRenderer 
+    public class HybridWebViewRenderer : ViewRenderer<HybridWebView, Android.Webkit.WebView>
     {
+        private Context _context;
+        public OnTaskCompleted onComplete;
+            
+        public HybridWebViewRenderer(Context context) : base(context)
+        {
+            _context = context;
+        }
 
+        protected override void OnElementChanged(ElementChangedEventArgs<HybridWebView> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control == null)
+            {
+                var webView = new Android.Webkit.WebView(_context);
+                webView.Settings.JavaScriptEnabled = true;
+                webView.SetWillNotDraw(true);
+                webView.AddJavascriptInterface(new SSFRJavaScriptInterface(), "SSFR");
+                webView.Settings.DomStorageEnabled = true;
+                var vebClient = new WebViewClient()
+                {
+
+                };
+                webView.SetWebViewClient(new WebViewClient()
+                {
+                    public override void OnLoadResource(Android.Webkit.WebView, string url)
+                    {
+                        base
+                    }
+                });
+            }
+        }
+        class SSFRJavaScriptInterface : Java.Lang.Object
+        {
+            [JavascriptInterface]
+            public void func(string url)
+            {
+                new Handler(Looper.MainLooper).Post(() =>
+                {
+                    
+                });
+            }
+        }
     }
+
+    public interface OnTaskCompleted
+    {
+        void onTaskCompleted(string vidUrl);
+        void onError();
+    }
+
+    
 }
