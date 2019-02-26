508d3dc5fb0fc36743f1c510af7748bcb6374cbd
Sergio Ferreras <Sergio_Ferreras_B@claro.com.do>
2019-02-26 17:43:58 -0400

    Refactor: Some Changes.

--- SSFR_Movies/SSFR_Movies/CustomRenderers/HybridWebView.cs
+++ SSFR_Movies/SSFR_Movies/CustomRenderers/HybridWebView.cs
@@ -5,7 +5,7 @@ using Xamarin.Forms;
 
 namespace SSFR_Movies.CustomRenderers
 {
-    public class HybridWebView : View
+    public class HybridWebView : WebView
     {
         public static readonly BindableProperty UriProperty = BindableProperty.Create(
             propertyName: "Uri",
@@ -13,22 +13,37 @@ namespace SSFR_Movies.CustomRenderers
             declaringType: typeof(HybridWebView),
             defaultValue: default(string));
 
-        public EventHandler OnFinishEH;
+        public event EventHandler<LoadUrlRequested> LoadRequest;
+
+        public event EventHandler OnFinishEH;
+
         public Action<string> Find;
+
         public string Uri
         {
             get => (string)GetValue(UriProperty);
             set => SetValue(UriProperty, value);
         }
 
-        private bool FindLink(string url)
-        {
-            return false;
-        }
-
         public void OnFinish()
         {
             OnFinishEH?.Invoke(this, EventArgs.Empty);
+        }
+
+        public void LoadUrl(string url)
+        {
+            EventHandler<LoadUrlRequested> handler = LoadRequest;
+            handler?.Invoke(this, new LoadUrlRequested(url));
+        }
+
+        public class LoadUrlRequested
+        {
+            public string Url { get; set; }
+
+            public LoadUrlRequested(string url)
+            {
+                this.Url = url;
+            }
         }
     }
 }
