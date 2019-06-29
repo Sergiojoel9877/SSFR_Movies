dac76fd7d0a95fe82d624df7ac9813eeb5861adc
Sergio Joel Ferreras <sergiojoel9877@hotmail.com>
2019-06-29 17:23:27 -0400

    refactor: Comment XGetter dependencies.

--- SSFR_Movies/SSFR_Movies.Android/CustomRenderers/HybridWebViewRenderer.cs
+++ SSFR_Movies/SSFR_Movies.Android/CustomRenderers/HybridWebViewRenderer.cs
@@ -2,8 +2,8 @@
 using Android.OS;
 using Android.Views;
 using Android.Webkit;
-using Com.Htetznaing.Xgetter;
-using Com.Htetznaing.Xgetter.Model;
+//using Com.Htetznaing.Xgetter;
+//using Com.Htetznaing.Xgetter.Model;
 using SSFR_Movies.CustomRenderers;
 using SSFR_Movies.Droid.CustomRenderers;
 using SSFR_Movies.Droid.Services;
@@ -11,23 +11,23 @@ using System;
 using System.Collections.Generic;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
-using static Com.Htetznaing.Xgetter.XGetter;
+//using static Com.Htetznaing.Xgetter.XGetter;
 
 [assembly: ExportRenderer(typeof(HybridWebView), typeof(HybridWebViewRenderer))]
 namespace SSFR_Movies.Droid.CustomRenderers
 {
-    public class HybridWebViewRenderer : ViewRenderer<HybridWebView, Android.Webkit.WebView>, IOnTaskCompleted
+    public class HybridWebViewRenderer : ViewRenderer<HybridWebView, Android.Webkit.WebView>/*,IOnTaskCompleted*/
     {
         readonly private Context _context;
-        readonly private XGetter xGetter;
-        //private string org;
-        private static IOnTaskCompleted onComplete;
+        //readonly private XGetter xGetter;
+        ////private string org;
+        //private static IOnTaskCompleted onComplete;
         readonly private string openload = "https?:\\/\\/(www\\.)?(openload|oload)\\.[^\\/,^\\.]{2,}\\/(embed|f)\\/.+";
 
         public HybridWebViewRenderer(Context context) : base(context)
         {
             _context = context;
-            xGetter = new XGetter(Context);
+            //xGetter = new XGetter(Context);
         }
 
         protected override Android.Webkit.WebView CreateNativeControl()
@@ -42,7 +42,7 @@ namespace SSFR_Movies.Droid.CustomRenderers
             if (e.NewElement != null && Control == null)
             {
                 var webView = CreateNativeControl();
-                xGetter.OnFinish(this);
+                //xGetter.OnFinish(this);
 #pragma warning disable 618 // This can probably be replaced with LinearLayout(LayoutParams.MatchParent, LayoutParams.MatchParent); just need to test that theory
                 webView.LayoutParameters = new global::Android.Widget.AbsoluteLayout.LayoutParams(LayoutParams.MatchParent, LayoutParams.MatchParent, 0, 0);
 #pragma warning restore 618
@@ -214,9 +214,9 @@ namespace SSFR_Movies.Droid.CustomRenderers
             throw new NotImplementedException();
         }
 
-        void IOnTaskCompleted.OnTaskCompleted(IList<XModel> p0, bool p1)
-        {
-            throw new NotImplementedException();
-        }
+        //void IOnTaskCompleted.OnTaskCompleted(IList<XModel> p0, bool p1)
+        //{
+        //    throw new NotImplementedException();
+        //}
     }
 }
