83bdf9edb47bff02f662b04dd50f0607c9b685ce
Sergio Joel Ferreras <sergiojoel9877@hotmail.com>
2019-08-08 20:35:59 -0400

    Refactor: Avoid return of new Uri if value is Null.

--- SSFR_Movies/SSFR_Movies/Converters/PosterImageUrlConverter.cs
+++ SSFR_Movies/SSFR_Movies/Converters/PosterImageUrlConverter.cs
@@ -9,7 +9,7 @@ namespace SSFR_Movies.Converters
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (value == null)
-                return new Uri("NoInternet.png");
+                return "NoInternet.png";
 
             return new Uri("https://image.tmdb.org/t/p/w370_and_h556_bestv2" + (string)value);
         }
