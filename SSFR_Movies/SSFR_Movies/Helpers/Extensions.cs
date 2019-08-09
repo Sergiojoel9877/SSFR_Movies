e2bbd6ed1c45d4b1c106fa83f346acb639cfa87f
Sergio Joel Ferreras <sergiojoel9877@hotmail.com>
2019-08-08 20:38:16 -0400

    Refactor: remove Tasl.Yield(). Use async version of InbokeOnUIThread.

--- SSFR_Movies/SSFR_Movies/Helpers/Extensions.cs
+++ SSFR_Movies/SSFR_Movies/Helpers/Extensions.cs
@@ -1,8 +1,5 @@
 ﻿//using SSFR_Movies.Data;
-using SSFR_Movies.Models;
-using System;
 using System.Threading.Tasks;
-using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Internals;
 
@@ -15,17 +12,15 @@ namespace SSFR_Movies.Helpers
         //to assign its value to the initial range of the animation.........
         public static async Task SetAnimation(this Label lbl)
         {
-            await Task.Yield();
+            await Device.InvokeOnMainThreadAsync(() =>
+            {
+                var right = new Animation(d => lbl.TranslationX = d, 350, -500);
 
-                Device.BeginInvokeOnMainThread(() =>
+                right.Commit(lbl, "Animation", 300, 7500, Easing.Linear, (d, b) =>
                 {
-                    var right = new Animation(d => lbl.TranslationX = d, 350, -500);
-
-                    right.Commit(lbl, "Animation", 300, 7500, Easing.Linear, (d, b) =>
-                    {
-                        lbl.TranslationX = 350;
-                    }, () => true);
-                });
+                    lbl.TranslationX = 350;
+                }, () => true);
+            });
         }
     }
 }
