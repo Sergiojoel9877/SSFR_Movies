292723cc2197819226fa34c8caba7736715a74f7
Sergio Joel Ferreras <sergiojoel9877@hotmail.com>
2019-08-08 21:34:34 -0400

    Refactor: Add Start page, to improve app starting performance.

---
+++ SSFR_Movies/SSFR_Movies/Views/Start.xaml.cs
@@ -0,0 +1,47 @@
+﻿using SSFR_Movies.Services;
+using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SSFR_Movies.Views
+{
+    public partial class Start : ContentPage
+    {
+        public Start()
+        {
+            InitializeComponent();
+
+            FireContainerCommand.Execute(null);
+        }
+
+        readonly Command fireContainerCommand = null;
+        Command FireContainerCommand
+        {
+            get => fireContainerCommand ?? (new Command(async () =>
+            {
+                ActIndicator.IsVisible = false;
+
+                ActIndicator.IsRunning = false;
+
+                Stack.IsVisible = true;
+
+                await ProBar.ProgressTo(.5, 200, Easing.Linear);
+
+                new Lazy<ContainerInitializer>(() => new ContainerInitializer()).Value.Initialize();
+
+                var mainPage = new Lazy<AppShell>(() => new AppShell());
+
+                Device.BeginInvokeOnMainThread(async () =>
+                {
+                    await ProBar.ProgressTo(100, 200, Easing.Linear);
+
+                    Stack.IsVisible = false;
+
+                    App.Current.MainPage = mainPage.Value;
+                });
+
+            }));
+        }
+    }
+}
